d56829f91b6d1c49fa9232350f6c9eae7fcd4285
sosuke-k <snoopies.drum@gmail.com>
2015-06-21 18:39:59 +0900

    add .editorconfig

--- chrome/chromeManager.go
+++ chrome/chromeManager.go
@@ -1,34 +1,34 @@
 package chrome
 
 import (
-  "os"
-  "os/exec"
-  "path"
-  "path/filepath"
-  "log"
-  "fmt"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
 )
 
 func Save(disPath string) {
-  execJXA("save", disPath)
+	execJXA("save", disPath)
 }
 
 func Restore(disPath string) {
-  execJXA("restore", disPath)
+	execJXA("restore", disPath)
 }
 
 /*
  * the parent directory of disPath must exist
  */
 func execJXA(cmd string, disPath string) {
-  srcDir, err := filepath.Abs(filepath.Dir(os.Args[0])) // i.e. $GOPATH/src/github.com/sosuke-k/hyena
-  if err != nil {
-    log.Fatal(err)
-  }
-  srcPath := path.Join(srcDir, "chrome/chrome_" + cmd + "_tab.applescript")
-  shCmd := "osascript"
-  args := []string{"-l", "JavaScript", srcPath, disPath}
-  if err = exec.Command(shCmd, args...).Run(); err != nil {
+	srcDir, err := filepath.Abs(filepath.Dir(os.Args[0])) // i.e. $GOPATH/src/github.com/sosuke-k/hyena
+	if err != nil {
+		log.Fatal(err)
+	}
+	srcPath := path.Join(srcDir, "chrome/chrome_"+cmd+"_tab.applescript")
+	shCmd := "osascript"
+	args := []string{"-l", "JavaScript", srcPath, disPath}
+	if err = exec.Command(shCmd, args...).Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
--- hyena.go
+++ hyena.go
@@ -1,140 +1,140 @@
 package main
 
 import (
-  "os"
-  "os/user"
+	"bufio"
+	"os"
+	"os/user"
 	"path"
-  "bufio"
-  // "fmt"
-  "log"
-  // "bytes"
-  "github.com/codegangsta/cli"
-  // "github.com/codeskyblue/go-sh"
-  // "github.com/kardianos/osext"
-  "github.com/sosuke-k/hyena/pm"
-  "github.com/sosuke-k/hyena/chrome"
+	// "fmt"
+	"log"
+	// "bytes"
+	"github.com/codegangsta/cli"
+	// "github.com/codeskyblue/go-sh"
+	// "github.com/kardianos/osext"
+	"github.com/sosuke-k/hyena/chrome"
+	"github.com/sosuke-k/hyena/pm"
 )
 
 var projects = []string{}
-var hyenaPath string //e.g. 'Users/name/.config/hyena'
+var hyenaPath string  //e.g. 'Users/name/.config/hyena'
 var configPath string //e.g. 'Users/name/.config/hyena/config.json'
-var srcDir string //e.g. 'Users/name/go/~~~'
+var srcDir string     //e.g. 'Users/name/go/~~~'
 
 func validError(errs ...error) error {
-  for i, _ := range errs {
-    if errs[i] != nil {
-      return errs[i]
-    }
-  }
-  return nil
+	for i, _ := range errs {
+		if errs[i] != nil {
+			return errs[i]
+		}
+	}
+	return nil
 }
 
 func init() {
-  hyenaPath = os.Getenv("HYENA_DIR_PATH")
-  if hyenaPath == "" {
-    usr, err := user.Current()
-    if err != nil {
-      log.Fatal(err)
-    }
-    hyenaPath = path.Join(usr.HomeDir, ".config/hyena")
-  }
-  configPath = path.Join(hyenaPath, "config.json")
+	hyenaPath = os.Getenv("HYENA_DIR_PATH")
+	if hyenaPath == "" {
+		usr, err := user.Current()
+		if err != nil {
+			log.Fatal(err)
+		}
+		hyenaPath = path.Join(usr.HomeDir, ".config/hyena")
+	}
+	configPath = path.Join(hyenaPath, "config.json")
 }
 
 func save(projectName string) {
-  projectPath := path.Join(hyenaPath, projectName)
-  chrome.Save(path.Join(projectPath, "chrome.json"))
+	projectPath := path.Join(hyenaPath, projectName)
+	chrome.Save(path.Join(projectPath, "chrome.json"))
 }
 
 func restore(projectName string) {
-  projectPath := path.Join(hyenaPath, projectName)
-  chrome.Restore(path.Join(projectPath, "chrome.json"))
+	projectPath := path.Join(hyenaPath, projectName)
+	chrome.Restore(path.Join(projectPath, "chrome.json"))
 }
 
 func main() {
-  app := cli.NewApp()
-  // app.EnableBashCompletion = true
-  app.Name = "hyena"
-  app.Usage = "see help"
-  app.Action = func(c *cli.Context) {
-    println("This is the tool like hyena...")
-    println("to get more info, command 'hyena help'")
-  }
+	app := cli.NewApp()
+	// app.EnableBashCompletion = true
+	app.Name = "hyena"
+	app.Usage = "see help"
+	app.Action = func(c *cli.Context) {
+		println("This is the tool like hyena...")
+		println("to get more info, command 'hyena help'")
+	}
 
-  app.Commands = []cli.Command{
-    {
-      Name:      "init",
-      Aliases:     []string{"i"},
-      Usage:     "initialize hyena",
-      Action: func(c *cli.Context) {
-        println("Do you create " + configPath + " ? y/[n]")
+	app.Commands = []cli.Command{
+		{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   "initialize hyena",
+			Action: func(c *cli.Context) {
+				println("Do you create " + configPath + " ? y/[n]")
 
-        scanner := bufio.NewScanner(os.Stdin)
-        scanner.Scan()
-        if err := scanner.Err(); err != nil {
-            println(os.Stderr, "reading standard input:", err)
-        }
-        a := scanner.Text()
-        if a == "y" {
-          pm.Init(configPath)
-          println("config file was created")
-        } else {
-          println("please input y")
-        }
-      },
-    }, // end init action definition
-    {
-      Name:      "ls",
-      Aliases:     []string{"l"},
-      Usage:     "show the list",
-      Action: func(c *cli.Context) {
-        projects = pm.Load(configPath)
-        for _, v := range projects {
-          println(v)
-        }
-      },
-    }, // end ls action definition
-    {
-      Name:      "add",
-      Aliases:     []string{"a"},
-      Usage:     "add the project",
-      Action: func(c *cli.Context) {
-        name := c.Args().First()
-        if name == "" {
-          println("please input project name")
-        } else {
-          pm.Add(configPath, name)
-          println("created new project named " + name)
-        }
-      },
-    }, // end add action definition
-    {
-      Name:      "save",
-      Aliases:     []string{"s"},
-      Usage:     "save the project",
-      Action: func(c *cli.Context) {
-        name := c.Args().First()
-        if name == "" {
-          println("please input project name")
-        } else {
-          save(name)
-        }
-      },
-    }, // end save action definition
-    {
-      Name:      "restore",
-      Aliases:     []string{"r"},
-      Usage:     "restore the project",
-      Action: func(c *cli.Context) {
-        name := c.Args().First()
-        if name == "" {
-          println("please input project name")
-        } else {
-          restore(name)
-        }
-      },
-    }, // end restore action definition
-  }
+				scanner := bufio.NewScanner(os.Stdin)
+				scanner.Scan()
+				if err := scanner.Err(); err != nil {
+					println(os.Stderr, "reading standard input:", err)
+				}
+				a := scanner.Text()
+				if a == "y" {
+					pm.Init(configPath)
+					println("config file was created")
+				} else {
+					println("please input y")
+				}
+			},
+		}, // end init action definition
+		{
+			Name:    "ls",
+			Aliases: []string{"l"},
+			Usage:   "show the list",
+			Action: func(c *cli.Context) {
+				projects = pm.Load(configPath)
+				for _, v := range projects {
+					println(v)
+				}
+			},
+		}, // end ls action definition
+		{
+			Name:    "add",
+			Aliases: []string{"a"},
+			Usage:   "add the project",
+			Action: func(c *cli.Context) {
+				name := c.Args().First()
+				if name == "" {
+					println("please input project name")
+				} else {
+					pm.Add(configPath, name)
+					println("created new project named " + name)
+				}
+			},
+		}, // end add action definition
+		{
+			Name:    "save",
+			Aliases: []string{"s"},
+			Usage:   "save the project",
+			Action: func(c *cli.Context) {
+				name := c.Args().First()
+				if name == "" {
+					println("please input project name")
+				} else {
+					save(name)
+				}
+			},
+		}, // end save action definition
+		{
+			Name:    "restore",
+			Aliases: []string{"r"},
+			Usage:   "restore the project",
+			Action: func(c *cli.Context) {
+				name := c.Args().First()
+				if name == "" {
+					println("please input project name")
+				} else {
+					restore(name)
+				}
+			},
+		}, // end restore action definition
+	}
 
-  app.Run(os.Args)
+	app.Run(os.Args)
 }

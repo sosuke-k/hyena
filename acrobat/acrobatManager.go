23f948c0e8b25b0b3a7b0131f721e93324108afb
sosuke-k <snoopies.drum@gmail.com>
2015-06-22 11:34:08 +0900

    add comment to exported function in acrobatManager.go

--- acrobat/acrobatManager.go
+++ acrobat/acrobatManager.go
@@ -1,34 +1,36 @@
 package acrobat
 
 import (
-  "os"
-  "os/exec"
-  "path"
-  "path/filepath"
-  "log"
-  "fmt"
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
 )
 
+// Save saves acrobat windows info to the config file at disPath
 func Save(disPath string) {
-  execJXA("save", disPath)
+	execJXA("save", disPath)
 }
 
+// Restore restores acrobat windows by the config file at disPath
 func Restore(disPath string) {
-  execJXA("restore", disPath)
+	execJXA("restore", disPath)
 }
 
 /*
  * the parent directory of disPath must exist
  */
 func execJXA(cmd string, disPath string) {
-  srcDir, err := filepath.Abs(filepath.Dir(os.Args[0])) // i.e. $GOPATH/src/github.com/sosuke-k/hyena
-  if err != nil {
-    log.Fatal(err)
-  }
-  srcPath := path.Join(srcDir, "acrobat/acrobat_" + cmd + "_doc.applescript")
-  shCmd := "osascript"
-  args := []string{"-l", "JavaScript", srcPath, disPath}
-  if err = exec.Command(shCmd, args...).Run(); err != nil {
+	srcDir, err := filepath.Abs(filepath.Dir(os.Args[0])) // i.e. $GOPATH/src/github.com/sosuke-k/hyena
+	if err != nil {
+		log.Fatal(err)
+	}
+	srcPath := path.Join(srcDir, "acrobat/acrobat_"+cmd+"_doc.applescript")
+	shCmd := "osascript"
+	args := []string{"-l", "JavaScript", srcPath, disPath}
+	if err = exec.Command(shCmd, args...).Run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}

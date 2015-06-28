84ee4295b388fbda509f87d47274e73543d82260
sosuke-k <snoopies.drum@gmail.com>
2015-06-29 02:15:57 +0900

    jxa.Execute call sh.Execute

--- util/jxa/jxa.go
+++ util/jxa/jxa.go
@@ -1,15 +1,13 @@
 package jxa
 
 import (
-	"bytes"
 	"fmt"
 	"os"
-	"os/exec"
 	"path"
 	"strings"
-	"unsafe"
 
 	"github.com/sosuke-k/hyena/util/log"
+	"github.com/sosuke-k/hyena/util/sh"
 )
 
 // Check checks if the application of identifier is running
@@ -31,54 +29,13 @@ func Check(identifier string) (isRunning bool) {
 }
 
 // Execute executes osascript with args
-func Execute(dir string, args []string) string {
+func Execute(dir string, args []string) (outString string) {
 	shCmd := "osascript"
 
-	hyenaLogger := logger.GetInstance()
-	hyenaLogger.Println("to execete " + shCmd)
-	for i := range args {
-		hyenaLogger.Println("  " + args[i])
-	}
 	fmt.Fprintln(os.Stdout, "to execete "+shCmd+" "+args[2])
-
-	cmd := exec.Command(shCmd, args...)
-	cmd.Dir = dir
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		hyenaLogger.Fatalln(err)
-	}
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		hyenaLogger.Fatalln(err)
-	}
-	if err := cmd.Start(); err != nil {
-		hyenaLogger.Fatalln(err)
-	}
 	fmt.Fprintln(os.Stdout, "waiting for "+shCmd+" command to finish...")
 
-	outBuf := new(bytes.Buffer)
-	outBuf.ReadFrom(stdout)
-	outBytes := outBuf.Bytes()
-	outString := *(*string)(unsafe.Pointer(&outBytes))
-	hyenaLogger.Println("to display " + shCmd + " stdout:")
-	hyenaLogger.Println(outString)
-
-	errBuf := new(bytes.Buffer)
-	errBuf.ReadFrom(stderr)
-	errBytes := errBuf.Bytes()
-	errString := *(*string)(unsafe.Pointer(&errBytes))
-	hyenaLogger.Println("to display " + shCmd + " stderr:")
-	hyenaLogger.Println(errString)
-
-	if err := cmd.Wait(); err != nil {
-		hyenaLogger.Printf("command finished with error: %v", err)
-		fmt.Fprintf(os.Stdout, "command finished with error: %v", err)
-		fmt.Fprintln(os.Stdout, "")
-		fmt.Fprintln(os.Stdout, "please see ~/.config/hyena/hyena.log")
-	} else {
-		hyenaLogger.Println("command finished")
-		fmt.Fprintln(os.Stdout, "command finished")
-	}
+	outString = sh.Execute(dir, shCmd, args)
 
 	return outString
 }

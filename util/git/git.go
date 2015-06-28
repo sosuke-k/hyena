d54861979f3c6ef2fc230f2105e9f997eac2ea67
sosuke-k <snoopies.drum@gmail.com>
2015-06-29 02:26:07 +0900

    add Init function and Commit function to git package

--- util/git/git.go
+++ util/git/git.go
@@ -1,43 +1,18 @@
 package git
 
-import (
-	"bytes"
-	"os/exec"
-	"unsafe"
+import "github.com/sosuke-k/hyena/util/sh"
 
-	"github.com/sosuke-k/hyena/util/log"
-)
+// Init execute git init in dir
+func Init(dir string) {
+	execute(dir, []string{"init"})
+}
 
-// Execute executes osascript with args
-func Execute(dir string, args []string) {
-	shCmd := "git"
+// Commit execute git add . and commit -m msg
+func Commit(dir string, msg string) {
+	execute(dir, []string{"add", "."})
+	execute(dir, []string{"commit", "-m", msg})
+}
 
-	hyenaLogger := logger.GetInstance()
-	hyenaLogger.Println("to execute " + shCmd)
-	for i := range args {
-		hyenaLogger.Println("  " + args[i])
-	}
-
-	cmd := exec.Command(shCmd, args...)
-	cmd.Dir = dir
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		hyenaLogger.Fatalln(err)
-	}
-	if err := cmd.Start(); err != nil {
-		hyenaLogger.Fatalln(err)
-	}
-
-	errBuf := new(bytes.Buffer)
-	errBuf.ReadFrom(stderr)
-	errBytes := errBuf.Bytes()
-	errString := *(*string)(unsafe.Pointer(&errBytes))
-	hyenaLogger.Println("to display " + shCmd + " log:")
-	hyenaLogger.Println(errString)
-
-	if err := cmd.Wait(); err != nil {
-		hyenaLogger.Printf("Command finished with error: %v", err)
-	} else {
-		hyenaLogger.Println("Command finished")
-	}
+func execute(dir string, args []string) {
+	sh.Execute(dir, "git", args)
 }

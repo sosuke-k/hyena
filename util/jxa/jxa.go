51b9e5b5e87b5f17cc7ba93d3246bad14984aabc
sosuke-k <snoopies.drum@gmail.com>
2015-06-29 00:27:05 +0900

    log stdout when executing osascript

--- util/jxa/jxa.go
+++ util/jxa/jxa.go
@@ -61,16 +61,27 @@ func Execute(dir string, args []string) {
 	if err != nil {
 		hyenaLogger.Fatalln(err)
 	}
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		hyenaLogger.Fatalln(err)
+	}
 	if err := cmd.Start(); err != nil {
 		hyenaLogger.Fatalln(err)
 	}
 	fmt.Fprintln(os.Stdout, "Waiting for "+shCmd+" command to finish...")
 
+	outBuf := new(bytes.Buffer)
+	outBuf.ReadFrom(stdout)
+	outBytes := outBuf.Bytes()
+	outString := *(*string)(unsafe.Pointer(&outBytes))
+	hyenaLogger.Println("to display " + shCmd + " stdout:")
+	hyenaLogger.Println(outString)
+
 	errBuf := new(bytes.Buffer)
 	errBuf.ReadFrom(stderr)
 	errBytes := errBuf.Bytes()
 	errString := *(*string)(unsafe.Pointer(&errBytes))
-	hyenaLogger.Println("to display " + shCmd + " log:")
+	hyenaLogger.Println("to display " + shCmd + " stderr:")
 	hyenaLogger.Println(errString)
 
 	if err := cmd.Wait(); err != nil {

9f8eaab18f590925603135c18d54e9e0141a6cce
rooa <hiroakihayash88@gmail.com>
2015-06-30 18:38:06 +0900

    Fix typo

--- app/preview/previewManager.go
+++ app/preview/previewManager.go
@@ -27,7 +27,7 @@ func execJXA(cmd string, disPath string) {
 	srcDir := path.Join(os.Getenv("GOPATH"), "src/github.com/sosuke-k/hyena/app/preview")
 	fileName := "preview_" + cmd + "_doc.applescript"
 	args := []string{"-l", "JavaScript", fileName, disPath}
-	fmt.Fprintln(os.Stdout, "Execeting osascript "+args[2])
+	fmt.Fprintln(os.Stdout, "Executing osascript "+args[2])
 	fmt.Fprintln(os.Stdout, "Waiting for the command to finish...")
 	jxa.Execute(srcDir, args)
 	fmt.Fprintln(os.Stdout, "Finished osascript "+args[2])
